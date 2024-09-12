415354fe84f4b8a7e6f96eef7f03b2ae8e51d2be
CodeNewbie-pradip <pradipalabade6442@gmail.com>
2024-09-12 18:17:12 +0530

    CodeChef 1736

---
+++ 1700-1799/Ball Game.cpp
@@ -0,0 +1,50 @@
+/*
+CodeChef Link               : https://www.codechef.com/problems/BALL_GAME
+CodeChef Problem difficulty : 1736
+CodeChef Related Topic      : Array
+*/
+//------------------------------Solution - in - C++ -----------------------------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define speed (ios_base::sync_with_stdio(false), cin.tie(NULL))
+#define vec vector<long long>
+
+int main(){
+    speed;
+    int T;
+    cin>>T; 
+    while(T--){
+        int n;
+        cin>>n;
+        vec a(n);
+        vec b(n);
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        for(int i=0; i<n; i++){
+            cin>>b[i];
+        }
+
+        vector<pair<ll, ll>>vp;
+        for(ll i=0; i<n; i++){
+            vp.push_back({a[i], b[i]});
+        }
+        
+        sort(vp.begin(), vp.end());
+        
+        long double prev=1e9;
+        int cnt=0;
+        for(int i=n-1; i>=0; i--){
+            long double curr=((long double)vp[i].first/vp[i].second);
+            if(curr<=prev){
+                cnt++;
+                prev=curr;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+}
+//--------------------------------------------------------------------------------------------------------------------
